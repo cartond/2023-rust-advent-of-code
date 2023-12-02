52bc3a20dbde28adf62ee6a5625ad34a7304e513
cartond <cartond93@gmail.com>
2023-12-01 22:26:41 -0500

    Part2

--- day01/src/bin/part2.rs
+++ day01/src/bin/part2.rs
@@ -1,60 +1,61 @@
-/*
-	1. put the example data in the test input (`let result = part1(....`)
-	2. put the example answer in the tests assert
-	3. do your work in fn part1
-	4. run `cargo test` to test
-	5. when satisfied, put the puzzle input data into input1.txt
-	6. run `cargo run --bin part1` to get the answer, submit.
-	7. copy all the code into part2.rs
-	8. ...
-*/
-
 fn main() {
 	let input = include_str!("./input1.txt");
-	let output = part1(input);
+	let output = part2(input);
 	dbg!(output);
 }
 
-fn part1(input: &str) -> i32{
-	let mut rolling_sum = 0;
-	// for each line
-	// get left most number (early break)
-	// get right most number (early break)
-	// combine to make XY
-	// print and add to running sum
+fn part2(input: &str) -> u32{
+	let mut sum = 0;
 	for input_line in input.lines() {
-		println!("{}", input_line);
-		let left_dig = get_digit(input_line, false);
-		let right_dig = get_digit(input_line, true);
-		let ans = format!("{}{}", left_dig, right_dig);
-		let number: i32 = ans.to_string().parse().expect("Not a valid number");
-		println!("made {}{} should == {} should == {}", left_dig, right_dig, ans, number);
-		rolling_sum = rolling_sum +  number
+		println!("line:	{}", input_line);
+		let num = process_input_line(input_line);
+		println!("num:	{}", num);
+		sum += num;
 	}
-	rolling_sum
+	sum
 }
 
-fn get_digit(input: &str, reverse: bool) -> char {
-	if reverse {
-		for c in input.chars().rev() {
-			// c now iterates over the characters in reverse order
-			if c.is_numeric() {
-				// print!("c: {}", c);
-				// print!("c...: {}", c.to_string().parse::<i32>().unwrap());
-				return c
-			}
-		}
-	}else {
-		for c in input.chars() {
-			if c.is_numeric() {
-				// print!("c: {}", c);
-				// print!("c...: {}", c.to_string().parse::<i32>().unwrap());
-				return c
-			}
-		}
-	}
-	panic!("[err]~~~:  No int found? reverse: {} with input: {}", reverse, input)
+fn process_input_line(input_line: &str) -> u32 {
+    let mut it = (0..input_line.len()).filter_map(|index| {
+        // example, "hione22...", index 2 is 'o'
+        // so reduced_line would be "one22...."
+        // so we chck if that starts with the text version of a number
+        let reduced_line = &input_line[index..];
+        let result = if reduced_line.starts_with("one") {
+            '1'
+        } else if reduced_line.starts_with("two") {
+            '2'
+        } else if reduced_line.starts_with("three") {
+            '3'
+        } else if reduced_line.starts_with("four") {
+            '4'
+        } else if reduced_line.starts_with("five") {
+            '5'
+        } else if reduced_line.starts_with("six") {
+            '6'
+        } else if reduced_line.starts_with("seven") {
+            '7'
+        } else if reduced_line.starts_with("eight") {
+            '8'
+        } else if reduced_line.starts_with("nine") {
+            '9'
+        } else {
+            reduced_line.chars().next().unwrap()
+        };
+
+        result.to_digit(10)
+    });
+
+    let first = it.next().expect("should be a number");
+
+    match it.last() {
+        Some(num) => format!("{first}{num}"),
+        None => format!("{first}{first}"),
+    }
+    .parse::<u32>()
+    .expect("should be a valid number")
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -63,7 +64,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = part1("two1nine
+        let result = part2("two1nine
 eightwothree
 abcone2threexyz
 xtwone3four
